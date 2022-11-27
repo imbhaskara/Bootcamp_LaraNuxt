39c45af600411979ca581d40b7e6a0fbd1ee4aae
Imam Bhaskara <imbhaskara@Imams-Mac-mini.local>
2022-11-27 15:44:44 +0700

    Add DB Relationship to Model

--- powerhuman-backend/app/Models/Employee.php
+++ powerhuman-backend/app/Models/Employee.php
@@ -2,10 +2,39 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Employee extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'gender',
+        'age',
+        'phone',
+        'photo',
+        'team_id',
+        'role_id',
+        'is_verified',
+        'verified_at',
+    ];
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
